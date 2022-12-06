ce6a94be9e066966996bcdc3caf58e966851ff79
yghorcastello <yghorcastello.backend@gmail.com>
2022-12-06 15:50:45 -0300

    Atualização de Projeto

--- pagamento/models.py
+++ pagamento/models.py
@@ -19,7 +19,7 @@ class Fornecedor(models.Model):
 
 class Pagamento(models.Model):
     fornecedor = models.ForeignKey('Fornecedor', related_name='pagamento', on_delete=models.CASCADE)
-    descricao = models.CharField(max_length=100)
+    descricao = models.CharField(max_length=90)
     data_emissao = models.DateField()
     data_vencimento = models.DateField()
     valor_original = models.FloatField(default=0)
